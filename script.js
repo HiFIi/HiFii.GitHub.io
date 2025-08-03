477c8092f7944e72a56d7668fbeed5dcc12ed7fe
Kyler <jeffreykyler6@gmail.com>
2025-08-03 14:52:30 -0700

    Revert "Update script.js"
    This reverts commit f593636617a78260f00381d2067edf16e174d9ea.

--- script.js
+++ script.js
@@ -190,8 +190,10 @@ window.onload = () => {
         animatedBackground.style.opacity = "var(--base-opacity)";
       }
     }
-
+    // --- END NEW LOGIC ---
   };
+
+  tabs.forEach((tab) => {
     tab.addEventListener("click", () => activateTab(tab));
     tab.addEventListener("keydown", (e) => {
       if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
